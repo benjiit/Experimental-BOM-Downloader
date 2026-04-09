8123a74131348fd912bf8b38bf68e2225329aa0f
Ben Warner <benbwarner@gmail.com>
2026-04-09 14:35:35 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ require([
         bomData: [],
 
         onLoad: function() {
-            // Using 'widget.setTitle' and 'widget.setBody' is the standard UWA way
             widget.setTitle("BOM Exporter");
+            // Standard string concatenation instead of template literals
             widget.setBody("Enter a Physical ID to begin.");
         },
 
@@ -24,6 +24,7 @@ require([
                         method: 'GET',
                         type: 'json',
                         onComplete: function(data) {
+                            // Using standard function(item) instead of arrow functions (item =>)
                             self.bomData = data.member.map(function(item) {
                                 return {
                                     Name: item.title || "No Title",
@@ -35,13 +36,9 @@ require([
                             self.renderTable();
                         },
                         onFailure: function(error) {
-                            // FIXED: Use widget.setBody instead of referencing a raw variable
                             widget.setBody("<p style='color:red;'>Error fetching data: " + error + "</p>");
                         }
                     });
-                },
-                onFailure: function(error) {
-                     widget.setBody("<p style='color:red;'>Error getting service URL:" + error + "</p>");
                 }
             });
         },
@@ -51,32 +48,36 @@ require([
             html += '<table style="width:100%; border-collapse: collapse; font-family: sans-serif;">';
             html += '<tr style="background:#3d82e3; color:white;"><th>Name</th><th>Rev</th><th>Qty</th></tr>';
             
-            this.bomData.forEach(function(item) {
+            for (var i = 0; i < this.bomData.length; i++) {
+                var item = this.bomData[i];
                 html += '<tr>';
                 html += '<td style="border-bottom:1px solid #ccc; padding:8px;">' + item.Name + '</td>';
                 html += '<td style="border-bottom:1px solid #ccc; padding:8px;">' + item.Revision + '</td>';
                 html += '<td style="border-bottom:1px solid #ccc; padding:8px;">' + item.Quantity + '</td>';
                 html += '</tr>';
-            });
+            }
             
             html += '</table>';
             html += '<br><button id="csvBtn" style="padding:10px; cursor:pointer;">Download CSV</button>';
             html += '</div>';
             
-            // FIXED: Standard UWA method to update content
             widget.setBody(html);
 
             var self = this;
-            document.getElementById('csvBtn').addEventListener('click', function() {
-                self.downloadCSV();
-            });
+            var btn = document.getElementById('csvBtn');
+            if (btn) {
+                btn.onclick = function() {
+                    self.downloadCSV();
+                };
+            }
         },
 
         downloadCSV: function() {
             var csvContent = "Name,Revision,Type,Quantity\n";
-            this.bomData.forEach(function(row) {
+            for (var j = 0; j < this.bomData.length; j++) {
+                var row = this.bomData[j];
                 csvContent += row.Name + "," + row.Revision + "," + row.Type + "," + row.Quantity + "\n";
-            });
+            }
 
             var blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
             var url = URL.createObjectURL(blob);
@@ -84,11 +85,8 @@ require([
             link.setAttribute("href", url);
             link.setAttribute("download", "BOM_Export.csv");
             link.click();
-            URL.revokeObjectURL(url);
         }
     };
 
-    // Initialize the widget events
-    widget.addEvent('onLoad', bomWidget.onLoad);
-    widget.addEvent('onRefresh', bomWidget.onLoad);
+    widget.addEvent('onLoad', function() { bomWidget.onLoad(); });
 });
